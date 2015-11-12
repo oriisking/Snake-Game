dafba3723cf8652c2eba28d4102fec071bd17c01
oriisking <oriisking@gmail.com>
2015-11-12 22:15:56 +0200

    I have added notes to Fruit.cs and Snake.cs, and added a validation method to Snake.cs

--- ConsoleApplication3/Fruit.cs
+++ ConsoleApplication3/Fruit.cs
@@ -9,15 +9,18 @@ namespace ConsoleApplication3
     {
         private Point fruit;
         private Random rnd = new Random();
+        //בונאי של פרי המציב אותו במקום רנדומאלי
         public Fruit()
         {
             this.fruit = new Point(rnd.Next(1,81),rnd.Next(1,26));
         }
+        //פעולה שמשנה את מיקום הפרי
         public void setLocation()
         {
             this.fruit.setX(rnd.Next(1, 81));
             this.fruit.setY(rnd.Next(1, 26));
         }
+        //בודק אם ראש הנחש נמצא באותו המיקום של הפרי
         public bool checkIfIsOnFruit(Snake snk)
         {
             if (snk.getHead().isEqual(this.fruit))
--- ConsoleApplication3/Snake.cs
+++ ConsoleApplication3/Snake.cs
@@ -8,20 +8,30 @@ namespace ConsoleApplication3
     class Snake
     {
         private List<Point> Nodes;
+        //בונה נחש ריק בו רק ראש, שממוקמת במקום רנדומאלי על המפה
         public Snake()
         {
             Random rnd = new Random();
             Point head = new Point(rnd.Next(1,81),rnd.Next(1,26));
             this.Nodes.Add(head);
         }
+        //מחזיר את רשימת החוליות שמרכיבות את הנחש
         public List<Point> getBuddy()
         {
             return this.Nodes;
         }
-
+        //מחזיר את הנקודה שבה נמצא ראש הנחש
         public Point getHead()
         {
             return this.Nodes.First<Point>();
+        }
+        //בודק אם הנחש עולה על עצמו
+        public bool checkIfIsOnself()
+        {
+            for (int i = 1; i < this.Nodes.Count-1; i++)
+                if (this.Nodes[i].isEqual(this.getHead()))
+                    return true;              
+            return false;
         }
     }
 }
