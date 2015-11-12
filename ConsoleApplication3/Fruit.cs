523600edc4c92d3fc2f439d5720f99f43e8395a8
oriisking <oriisking@gmail.com>
2015-11-12 17:33:43 +0200

    First Upload

---
+++ ConsoleApplication3/Fruit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Fruit
+    {
+        private Point fruit;
+        private Random rnd = new Random();
+        public Fruit()
+        {
+            this.fruit = new Point(rnd.Next(1,81),rnd.Next(1,26));
+        }
+        public void setLocation()
+        {
+            this.fruit.setX(rnd.Next(1, 81));
+            this.fruit.setY(rnd.Next(1, 26));
+        }
+        public bool checkIfIsOnFruit(Snake snk)
+        {
+            if (snk.getBuddy().First<Point>().isEqual(this.fruit))
+                return true;
+            return false;
+            
+        }
+    }
+}
---
+++ ConsoleApplication3/Snake.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Snake
+    {
+        private List<Point> Nodes;
+        public Snake()
+        {
+            Random rnd = new Random();
+            Point head = new Point(rnd.Next(1,81),rnd.Next(1,26));
+            this.Nodes.Add(head);
+        }
+        public List<Point> getBuddy()
+        {
+            return this.Nodes;
+        }
+    }
+}
