4d22cca7e83463dbb8a89625b9cd46a4372b621c
oriisking <oriisking@gmail.com>
2015-11-14 23:30:25 +0200

    Added Movements

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Timers;
+using System.Threading;
 using System.Web;
 
 public enum direction { Up, Down, Left, Right };
@@ -11,49 +11,35 @@ namespace ConsoleApplication3
 {
     class Program
     {
-        static Timer tm = new Timer(1000.0);
+        static ConsoleKey cki = new ConsoleKey();
         static Snake snk = new Snake();
         static Fruit fru = new Fruit();
-        static ConsoleKey cki = new ConsoleKey();
-        static void makeMove()
+        static Thread thr = new Thread(new ThreadStart(checkTheKey));
+        public static void checkTheKey()
         {
-            if (Console.KeyAvailable)
-                cki = Console.ReadKey().Key;
-            do
+            while (true)
             {
-                snk.makeASingleMove(direction.Right);
-            } while (cki == ConsoleKey.RightArrow && snk.getBuddy()[0].getX() < 100);
-            do
+                if (Console.KeyAvailable)
+                    cki = Console.ReadKey().Key;
+            }
+        }
+        public static void makeMove()
+        {
+            switch (cki)
             {
-                snk.makeASingleMove(direction.Left);
-            } while (cki == ConsoleKey.LeftArrow);
-            do
-            {
-                snk.makeASingleMove(direction.Up);
-            } while (cki == ConsoleKey.UpArrow);
-            do
-            {
-                snk.makeASingleMove(direction.Down);
-            } while (cki == ConsoleKey.DownArrow);    
-            //switch (cki)
-            //{
-
-            //    case ConsoleKey.DownArrow:
-            //        snk.makeASingleMove(direction.Down);
-            //        break;
-            //    case ConsoleKey.LeftArrow:
-            //        snk.makeASingleMove(direction.Left);
-            //        break;
-            //    case ConsoleKey.RightArrow:
-            //        snk.makeASingleMove(direction.Right);
-            //        break;
-            //    case ConsoleKey.UpArrow:
-            //        snk.makeASingleMove(direction.Up);
-            //        break;
-
-            //}
-            
-            
+                case ConsoleKey.DownArrow:
+                    snk.makeASingleMove(direction.Down);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    snk.makeASingleMove(direction.Left);
+                    break;
+                case ConsoleKey.RightArrow:
+                    snk.makeASingleMove(direction.Right);
+                    break;
+                case ConsoleKey.UpArrow:
+                    snk.makeASingleMove(direction.Up);
+                    break;
+            }
         }
         public static void renderSnake()
         {
@@ -65,32 +51,25 @@ namespace ConsoleApplication3
             Console.SetCursorPosition(fru.getLocation().getX(), fru.getLocation().getY());
             Console.Write("O");
         }
-   
-        static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+
+        static void OnTimedEvent(Object obj)
         {
-            
             Console.Clear();
             renderSnake();
             makeMove();
         }
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-
-            
             Console.WindowHeight = 50;
             Console.WindowWidth = 100;
-
-            
             Console.BufferHeight = Console.WindowHeight;
             Console.BufferWidth = Console.WindowWidth;
-            cki = ConsoleKey.UpArrow;
+            thr.Start();
+            AutoResetEvent are = new AutoResetEvent(true);
+            TimerCallback tcb = OnTimedEvent;
+            Timer tmr = new Timer(tcb, are, 1000, 100);
             fru.setLocation();
-            tm.Elapsed += OnTimedEvent;
-            tm.Start();
-            Console.Read();
-
         }
-        
     }
 }
